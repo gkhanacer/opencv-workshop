093dd385bed14c6d3180d2e7403610fd3d4da4de
Gokhan Acer <gokhan.acer@arcelik.com>
2021-04-17 18:06:40 +0300

    first

---
+++ 01_image/read_image.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# READ IMAGE
+data_path = "/home/gokhan/opencv-workshop/data/"
+img = cv2.imread(data_path + "starry_night.jpg")
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv2.imshow("Display window", img)
+k = cv2.waitKey(0)
+
+if k == ord("s"):
+    cv2.imwrite("starry_night.png", img)
+
+
